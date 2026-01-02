8989a1be631dd730665628870d31499915313ce8
Johannes Rutzmoser <johannes.rutzmoser@posteo.de>
2026-01-02 15:43:09 +0100

    Refactor test_modbus script

--- custom_components/weishaupt_modbus/test_modbus.py
+++ custom_components/weishaupt_modbus/test_modbus.py
@@ -1,9 +1,25 @@
-"""Scan all possible modbus registers for a value."""
+"""Scan all possible modbus registers for a value and write them into the register.txt file"""
 
 import asyncio
 
 from pymodbus import ExceptionResponse, ModbusException, pymodbus_apply_logging_config
 import pymodbus.client as ModbusClient
+
+
+async def scan_registers(registers, register_read_function, *args, **kwargs):
+    """
+    Scan the registers using the given register_read_function
+    and store the results in a dictionary which gets returned.
+    """
+    register_dict = {}
+    for register in registers:
+        try:
+            rr = await register_read_function(register, *args, **kwargs)
+            if len(rr.registers) > 0:
+                register_dict[register] = rr.registers[0]
+        except ModbusException as exc:
+            print("Exception raised:", exc)
+    return register_dict
 
 
 async def main():  # noqa: D103
@@ -16,96 +32,46 @@ async def main():  # noqa: D103
     )
     await client.connect()
 
-    # binary_out = range(1, 9999)
-    # binary_in = range(10001, 19999)
-    binary_out = []
-    binary_in = []
-    input_register = range(30001, 39999)
-    holding_register = range(40001, 49999)
+    binary_out = []  # range(1, 10_000)
+    binary_in = []  # range(10_001, 20_000)
+    input_registers = range(30_001, 40_000)
+    holding_registers = range(40_001, 50_000)
 
-    file = open(file="register.txt", mode="w", encoding="UTF-8")  # noqa: ASYNC230, PTH123
-
-    file.write("Binary out\n\n")
-
-    for register in binary_out:
-        try:
-            rr = await client.read_coils(register, 1, device_id=1)
-            if len(rr.registers) > 0:
-                val = rr.registers[0]
-                print(rr)
-        except ModbusException as exc:
-            val = exc
-            print(val)
-        if rr.isError():
-            val = rr
-        if isinstance(rr, ExceptionResponse):
-            val = rr
-
-        file.write(str(register) + ";" + str(val) + "\n")
-
-    file.write("Binary in: \n\n")
-
-    for register in binary_in:
-        writeit = False
-        try:
-            rr = await client.read_coils(register, 1, device_id=1)
-            print(rr)  # noqa: T201
-        except ModbusException as exc:
-            val = exc
-            print(val)  # noqa: T201
-        if rr.isError() or isinstance(rr, ExceptionResponse):
-            val = rr
-            writeit = False
-        elif len(rr.registers) > 0:
-            val = rr.registers[0]
-            writeit = True
-
-        if writeit is True:
-            file.write(str(register) + ";" + str(val) + "\n")
-
-    file.write("Input Register: \n\n")
-
-    for register in input_register:
-        writeit = False
-        try:
-            rr = await client.read_input_registers(register, device_id=1)
-            print(rr)  # noqa: T201
-        except ModbusException as exc:
-            val = exc
-            print(val)  # noqa: T201
-        if rr.isError() or isinstance(rr, ExceptionResponse):
-            val = rr
-            writeit = False
-        elif len(rr.registers) > 0:
-            val = rr.registers[0]
-            writeit = True
-
-        if writeit is True:
-            file.write(str(register) + ";" + str(val) + "\n")
-
-    file.write("Holding Register: \n\n")
-
-    for register in holding_register:
-        writeit = False
-        try:
-            rr = await client.read_holding_registers(register, device_id=1)
-            print(rr)  # noqa: T201
-        except ModbusException as exc:
-            val = exc
-            print(val)  # noqa: T201
-        if rr.isError() or isinstance(rr, ExceptionResponse):
-            val = rr
-            writeit = False
-        elif len(rr.registers) > 0:
-            val = rr.registers[0]
-            writeit = True
-
-        if writeit is True:
-            file.write(str(register) + ";" + str(val) + "\n")
+    binary_out_found = await scan_registers(
+        binary_out, client.read_coils, 1, device_id=1
+    )
+    binary_in_found = await scan_registers(binary_in, client.read_coils, 1, device_id=1)
+    input_registers_found = await scan_registers(
+        input_registers, client.read_input_registers, device_id=1
+    )
+    holding_registers_found = await scan_registers(
+        holding_registers, client.read_holding_registers, device_id=1
+    )
 
     client.close()
 
+    file = open(file="register.txt", mode="w", encoding="UTF-8")  # noqa: ASYNC230, PTH123
+
+    file.write("Binary out:\n")
+    for register, val in binary_out_found.items():
+        file.write(f"{register};{val}\n")
+
+    file.write("Binary in:\n")
+    for register, val in binary_in_found.items():
+        file.write(f"{register};{val}\n")
+
+    file.write("Input Registers:\n")
+    for register, val in input_registers_found.items():
+        file.write(f"{register};{val}\n")
+
+    file.write("Holding Registers:\n")
+    for register, val in holding_registers_found.items():
+        file.write(f"{register};{val}\n")
+
+    return
+
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     loop.run_until_complete(main())
